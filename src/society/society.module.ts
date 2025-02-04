3f5f4bd9272979086dcdb1196dbcd79872d92b9b
DevLarry <olanrewajusamad070@gmail.com>
2025-02-04 21:34:45 +0100

    Include boilerplate for Society feature

--- src/society/society.module.ts
+++ src/society/society.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SocietyService } from './society.service';
 import { SocietyController } from './society.controller';
+import { PrismaService } from 'src/prisma.service';
 
 @Module({
   controllers: [SocietyController],
-  providers: [SocietyService],
+  providers: [SocietyService, PrismaService],
 })
 export class SocietyModule {}
--- src/society/society.service.ts
+++ src/society/society.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { CreateSocietyDto } from './dto/create-society.dto';
 import { UpdateSocietyDto } from './dto/update-society.dto';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class SocietyService {
+  constructor(private prismaService: PrismaService){}
   create(createSocietyDto: CreateSocietyDto) {
     return 'This action adds a new society';
   }
